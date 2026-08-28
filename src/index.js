8918c77a6d44c420a0aa592c2c300b7c8541b34a
polloMaloXd <nilton_pv@hotmail.com>
2026-08-28 18:40:13 -0500

    Agregando la lista de reproducción con sus portadas

--- src/index.js
+++ src/index.js
@@ -4,14 +4,72 @@ const artistName = document.getElementById('artist');
 const progressBar = document.getElementById('progress');
 const currentTim = document.getElementById('time');
 const durationSong = document.getElementById('duration');
+const portadaSong = document.getElementById('portada')
 
 const playBtn = document.getElementById('play');
 
-let playlist = [{
-  url: "music/Zafiro Rap - Siempre Unidos.mp3",
-  name: "Zafiro Rap",
-  title: "Siempre Unidos",
-}];
+let playlist = [
+  {
+    url: "../assets/music/Danny Ocean - Me Rehúso.mp3",
+    name: "Danny Ocean",
+    title: "Me Rehúso",
+    img: "../assets/images/album/danny_ocean.webp",
+  },
+  {
+    url: "../assets/music/Dragon & Caballero - Fruta Prohibida.mp3",
+    name: "Dragon & Caballero",
+    title: "Fruta Prohibida",
+    img: "../assets/images/album/dragon_y_caballero.webp",
+  },
+  {
+    url: "../assets/music/Feid - LUNA.mp3",
+    name: "Feid",
+    title: "Luna",
+    img: "../assets/images/album/feid.webp",
+  },
+  {
+    url: "../assets/music/Joey Montana - Sin Ti.mp3",
+    name: "Joey Montana",
+    title: "Sin Ti",
+    img: "../assets/images/album/joey_montana.webp",
+  },
+  {
+    url: "../assets/music/Kevin El Autorizado - El Villano.mp3",
+    name: "Kevin El Autorizado",
+    title: "El Villano",
+    img: "../assets/images/album/kevin_el_autorizado.webp",
+  },
+  {
+    url: "../assets/music/La Secta - La Locura Automática (Reggaeton).mp3",
+    name: "La Secta",
+    title: "La Locura Automática",
+    img: "../assets/images/album/la_secta.webp",
+  },
+  {
+    url: "../assets/music/Marc Segui - Tiroteo (Remix).mp3",
+    name: "Marc Segui",
+    title: "Tiroteo (Remix)",
+    img: "../assets/images/album/marc_segui.webp",
+  },
+  {
+    url: "../assets/music/Rkm & Ken-y - Llorarás.mp3",
+    name: "Rkm & Ken-y",
+    title: "Llorarás",
+    img: "../assets/images/album/rakim_y_ken.webp",
+  },
+  {
+    url: "../assets/music/She - A 700 Km (Con Elena).mp3",
+    name: "She",
+    title: "A 700 Km",
+    img: "../assets/images/album/she_y_elena.webp",
+  },
+  {
+    url: "../assets/music/Zafiro Rap - Siempre Unidos.mp3",
+    name: "Zafiro Rap",
+    title: "Siempre Unidos",
+    img: "../assets/images/album/zafiro_rap.webp",
+  },
+];
 
 let indexSong = 0;
 
@@ -19,6 +77,7 @@ function loadSong(index) {
   songTitle.textContent = index.title;
   artistName.textContent = index.name;
   audioPlayer.src = index.url;
+  portadaSong.src = index.img;
 }
 
 loadSong(playlist[indexSong])
